f8dd2b6221b1e57a809190f8e7811fe8c605e5c0
Evgenii S. <sl0vv.me@gmail.com>
2025-07-08 00:11:53 +0400

    Update demo base

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import glsl from 'vite-plugin-glsl';
 
 export default {
     root: ".",
+    base: "./",
     plugins: [
         glsl()
     ],
     server: {
         port: 3000,
         open: true
+    },
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true
     }
 };
